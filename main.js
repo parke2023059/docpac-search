cf1e3ff2da8bb8b06f12faa109b28e3277b7838b
Leniece <71403335+LenieceB1@users.noreply.github.com>
2022-02-07 14:40:16 -0500

    Csv to json test 1

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express()
 app.set('view engine', 'ejs')
+const csvToJson = require('convert-csv-to-json');
 const port = 1234
 const localhost = '127.0.0.1'
+const fs = require('fs');
+app.use(express.urlencoded({extended:true}))
 
+
+
+let json = csvToJson.getJsonFromCsv("changes.csv");
+for(let i=0; i<json.length;i++){
+    console.log(json[i]);
+}
 
 
 app.get('/', function(req,res){
@@ -11,10 +20,28 @@ app.get('/', function(req,res){
 })
 
 app.get('/getinfo', function(req,res){
-  res.render()
+  res.render('getData.ejs')
+})
+
+app.get('/adddata', function(req,res){
+  res.render('addData.ejs')
+})
+
+app.get('/dates', function(req,res){
+  res.render('datesList.ejs')
 })
 
 
+app.get('/dev', function(req,res){
+  res.render('dev.ejs')
+})
+
+/*app.post('/dev', function(req,res){
+  const inData = req.body.addDate
+  const dates = []
+  dates.push(inData)
+  stringData = JSON.stringify(dates)
+  fs.writeFileSync('dates.json', stringData)*/
 
 app.listen(port, localhost, function(){
   console.log("Server Status: Functional");

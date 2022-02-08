8dc4e0121a69587a375b0705663612c4fe65159f
parke2023059 <parke2023059@live.ytech.edu>
2022-02-08 10:47:18 -0500

    5

--- main.js
+++ main.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const app = express()
 app.set('view engine', 'ejs')
-const csvToJson = require('convert-csv-to-json');
 const port = 1234
 const localhost = '127.0.0.1'
 const fs = require('fs');
 app.use(express.urlencoded({extended:true}))
 
-let inputfile = 'changes.csv';
-let outputfile = 'changes.json';
-csvToJson.generateJsonFileFromCsv(inputfile,outputfile);
-
-function conversion(inputfile, outputfile) {
-  let input = inputfile;
-  let output = outputfile;
-  csvToJson.generateJsonFileFromCsv(input,output);
-  console.log('wrote to file')
-}
-
-conversion('Docpac Data.csv', 'docpacdata.json')
-
-conversion('goals.csv', 'goals.json')
-
-conversion('inc doc.csv', 'incdoc.json')
-
-conversion('req doc.csv', 'reqdoc.json')
-
-conversion('changes.csv', 'changes.json')
 
 
 app.get('/', function(req,res){
@@ -50,12 +29,16 @@ app.get('/dev', function(req,res){
   res.render('dev.ejs')
 })
 
-/*app.post('/dev', function(req,res){
-  const inData = req.body.addDate
-  const dates = []
-  dates.push(inData)
-  stringData = JSON.stringify(dates)
-  fs.writeFileSync('dates.json', stringData)*/
+app.get('/addgoals', function(req,res){
+  res.render('goals.ejs')
+})
+
+
+app.get('/incdoc', function(req,res){
+  res.render('includedDocumentation.ejs')
+})
+
+
 
 app.listen(port, localhost, function(){
   console.log("Server Status: Functional");
